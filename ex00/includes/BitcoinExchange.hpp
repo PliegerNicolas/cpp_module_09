8f823140a26fa6a3b5229d783e0f67b9b22296aa
Nicolas PLIEGER <plieger05@gmail.com>
2023-09-04 11:20:26 +0200

    Open file.

---
+++ ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nplieger <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 10:48:54 by nplieger          #+#    #+#             */
+/*   Updated: 2023/09/04 11:12:31 by nplieger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#pragma once
+
+#include "Colors.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class	BitcoinExchange
+{
+	private:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+
+		/* Member functions */
+
+	protected:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+
+		/* Member functions */
+
+	public:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+		BitcoinExchange(void);
+		//BitcoinExchange(void);
+
+		BitcoinExchange(const BitcoinExchange &other);
+		BitcoinExchange	&operator=(const BitcoinExchange &other);
+
+		~BitcoinExchange(void);
+
+		/* Member functions */
+};
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,11 +6,52 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 13:29:55 by nicolas           #+#    #+#             */
-/*   Updated: 2023/09/02 13:30:02 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/09/04 11:19:28 by nplieger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "BitcoinExchange.hpp"
 
-int	main(void)
+static bool	verifyArguments(int argc, char **argv)
 {
+	if (argc != 1)
+	{
+		std::cerr << RED;
+		std::cerr << "Error: 1 argument expected, " << argc << " recieved.";
+		std::cerr << WHITE << std::endl;
+		return (1);
+	}
+	else if (!argv || !**argv)
+	{
+		std::cerr << RED;
+		std::cerr << "Error: empty argument passed.";
+		std::cerr << WHITE << std::endl;
+		return (1);
+	}
 	return (0);
+}
+
+static bool	openFile(std::fstream &file, char *filename)
+{
+	file.open(filename);
+	if (file.fail())
+	{
+		std::cerr << RED;
+		std::cerr << "Error: " << filename << " isn't available";
+		if (errno == EACCES)
+			std::cerr << " (Permission denied)";
+		std::cerr << "." << WHITE << std::endl;
+		return (1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	std::fstream	file;
+
+	if (verifyArguments(--argc, ++argv))
+		return (1);
+	if (openFile(file, *argv))
+		return (1);
+	return (file.close(), 0);
 }
