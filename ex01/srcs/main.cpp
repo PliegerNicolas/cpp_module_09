12022d84e39d52d1d2455e0a161a55050ef29549
PliegerNicolas <plieger05@gmail.com>
2023-09-07 11:44:47 +0200

    Add link

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 09:42:35 by nicolas           #+#    #+#             */
-/*   Updated: 2023/09/07 10:51:06 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/09/07 11:44:39 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "RPN.hpp"
@@ -30,6 +30,8 @@ static bool	verifyArguments(int argc, char **argv)
 	return (0);
 }
 
+// https://paodayag.dev/reverse-polish-notation-js-parser/
+// Link can be useful to check but it's not sure if it's fail proof
 int	main(int argc, char **argv)
 {
 	if (verifyArguments(--argc, ++argv))
